148c97917cdfe738f72617dc0851930124d8e5b3
Daiwiik Harihar <daiwiikharihar17147@gmail.com>
2025-09-14 05:38:13 +0200

    Added Vercel Analytics

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
+import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import ExpertiseSection from "@/components/ExpertiseSection";
 import ServicesSection from "@/components/ServicesSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+
 
 const Index = () => {
   return (
