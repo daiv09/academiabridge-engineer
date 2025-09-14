337c2b0e3e593c44c5cb9a7d5708378c12eefc44
Daiwiik Harihar <daiwiikharihar17147@gmail.com>
2025-09-14 05:41:13 +0200

    Added Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
       </BrowserRouter>
     </TooltipProvider>
     <Analytics />
+    <SpeedInsights />
   </QueryClientProvider>
 );
 
